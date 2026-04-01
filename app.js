4c74b53573cb6f3a8b35b725f1bf8aa696b8b1f4
Chirantan Mane <chirantanmane0@gmail.com>
2026-04-01 19:09:16 +0530

    init database

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import express from "express";
-const app = express();
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import Listing from "./models/listing.js";
 dotenv.config({ quiet: true });
 const port = process.env.PORT || 3000;
 const MONGO_URL = process.env.MONGO_URL;
-
+const app = express();
 async function main() {
   await mongoose.connect(MONGO_URL);
 }
@@ -14,10 +14,6 @@ main()
     console.log("connected to database");
   })
   .catch((err) => console.log(err));
-import Listing from "./models/listing.js";
-
-
-Listing.insertMany(data);
 
 app.listen(port, () => {
   console.log("server started easily on port 3000");
